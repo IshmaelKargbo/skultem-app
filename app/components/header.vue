3331eb8247a8467be893e71cdb4de3104621bd39
Dev Isho <ishmaelkargbo5@outlook.com>
2026-04-04 16:47:31 +0000

    fix error

--- app/components/header.vue
+++ app/components/header.vue
@@ -1,21 +1,23 @@
 <template>
-    <div class="bg-white w-full border-b border-gray-200">
-        <div class="container mx-auto md:p-1.5 py-2 md:px-5 px-4  flex items-center justify-between">
-            <div class="flex space-x-1.5 items-center">
-                <MenuDrawer class="md:hidden block" />
-                <div>
-                    <p v-if="title" class="text-lg font-display font-semibold">{{ title }}</p>
-                    <USkeleton v-else class="h-4 w-44" />
+    <ClientOnly>
+        <div class="bg-white w-full border-b border-gray-200">
+            <div class="container mx-auto md:p-1.5 py-2 md:px-5 px-4  flex items-center justify-between">
+                <div class="flex space-x-1.5 items-center">
+                    <MenuDrawer class="md:hidden block" />
+                    <div>
+                        <p v-if="title" class="text-lg font-display font-semibold">{{ title }}</p>
+                        <USkeleton v-else class="h-4 w-44" />
+                    </div>
                 </div>
-            </div>
-            <div class="flex items-center space-x-2">
-                <div class="flex space-x-2">
-                    <UButton to="/notifications" variant="ghost" :icon="BELL_ICON" />
+                <div class="flex items-center space-x-2">
+                    <div class="flex space-x-2">
+                        <UButton to="/notifications" variant="ghost" :icon="BELL_ICON" />
+                    </div>
+                    <Me class="hidden md:block" />
                 </div>
-                <Me class="hidden md:block" />
             </div>
         </div>
-    </div>
+    </ClientOnly>
 </template>
 <script setup lang="ts">
 const { title } = storeToRefs(useAppStore())
